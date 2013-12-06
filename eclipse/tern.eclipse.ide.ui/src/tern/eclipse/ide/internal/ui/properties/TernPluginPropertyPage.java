65c0283ba7d0658a8dce834d63c149cef22e9d0c
angelozerr <angelo.zerr@gmail.com>
2013-12-06 20:19:40 +0100

    Initialize Tern (main+plugin) properties page.

---
+++ eclipse/tern.eclipse.ide.ui/src/tern/eclipse/ide/internal/ui/properties/TernPluginPropertyPage.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Angelo ZERR.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:      
+ *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *******************************************************************************/
+package tern.eclipse.ide.internal.ui.properties;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+/**
+ * Tern Plugin page for project properties.
+ * 
+ */
+public class TernPluginPropertyPage extends PropertyPage implements
+		IWorkbenchPropertyPage {
+
+	public static final String PROP_ID = "tern.eclipse.ide.internal.ui.properties.TernPluginPropertyPage";
+
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayoutData(new GridData(4, 4, true, true));
+		composite.setLayout(new GridLayout());
+
+		return composite;
+	}
+
+}
