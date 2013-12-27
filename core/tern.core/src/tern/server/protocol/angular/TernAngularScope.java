c7eac23cfc33f5134af506da67eb34c7ad78b5a8
angelozerr <angelo.zerr@gmail.com>
2013-12-27 23:15:25 +0100

    Manage ngModel in the scope.

--- core/tern.core/src/tern/server/protocol/angular/TernAngularScope.java
+++ core/tern.core/src/tern/server/protocol/angular/TernAngularScope.java
@@ -1,5 +1,8 @@
 package tern.server.protocol.angular;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -25,4 +28,45 @@ public class TernAngularScope extends JSONObject {
 		}
 		return controllers;
 	}
+
+	public void addModel(String model) {
+		// JSONObject json = new JSONObject();
+		if (model.indexOf('.') == -1) {
+			getProps().put(model, model);
+		}
+	}
+
+	public JSONObject getProps() {
+		JSONObject props = (JSONObject) super.get("props");
+		if (props == null) {
+			props = new JSONObject();
+			super.put("props", props);
+		}
+		return props;
+	}
+
+	public void addRepeat(String expression) {
+		Pattern pattern = Pattern
+				.compile("^\\s*(.+)\\s+in\\s+(.*?)\\s*(\\s+track\\s+by\\s+(.+)\\s*)?$");
+		Matcher matcher = pattern.matcher(expression);
+		while (matcher.find()) {
+			String lhs = matcher.group(1);
+			String rhs = matcher.group(2);
+
+			Pattern pattern2 = Pattern
+					.compile("^(?:([\\$\\w]+)|\\(([\\$\\w]+)\\s*,\\s*([\\$\\w]+)\\))$");
+			Matcher matcher2 = pattern2.matcher(lhs);
+			while (matcher2.find()) {
+				String valueIdentifier = matcher.group(3) != null ? matcher
+						.group(3) : matcher.group(1);
+				String keyIdentifier = matcher.group(2);
+
+				JSONObject repeat = new JSONObject();
+				repeat.put("repeat", keyIdentifier);
+				getProps().put(valueIdentifier, repeat);
+			}
+
+		}
+	}
+
 }
