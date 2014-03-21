9f2b7d70db0a8d35f1006d6f95aa31173ac7de43
angelozerr <angelo.zerr@gmail.com>
2014-03-21 10:30:07 +0100

    Fix https://github.com/angelozerr/angularjs-eclipse/issues/39

---
+++ core/tern.core.tests/src/tern/server/nodejs/angular/NodejsIssue39.java
@@ -0,0 +1,28 @@
+/**
+ *  Copyright (c) 2013-2014 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
+package tern.server.nodejs.angular;
+
+import tern.TernException;
+import tern.TernProject;
+import tern.server.ITernServer;
+import tern.server.nodejs.NodejsTernServerFactory;
+import tern.server.protocol.angular.AbstractAngularControllerCompletionTest;
+import tern.server.protocol.angular.Issue39;
+
+public class NodejsIssue39 extends
+		Issue39 {
+
+	@Override
+	protected ITernServer createServer(TernProject project)
+			throws TernException {
+		return NodejsTernServerFactory.createServer(project);
+	}
+}
---
+++ core/tern.core.tests/src/tern/server/protocol/angular/Issue39.java
@@ -0,0 +1,66 @@
+/**
+ *  Copyright (c) 2013-2014 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
+package tern.server.protocol.angular;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import tern.TernException;
+import tern.angular.AngularType;
+import tern.angular.protocol.completions.TernAngularCompletionItem;
+import tern.angular.protocol.completions.TernAngularCompletionsQuery;
+import tern.server.protocol.TernDoc;
+
+/**
+ * Tests with tern angular controller completion.
+ * 
+ */
+public abstract class Issue39 extends AbstractTernServerAngularTest {
+
+	@Test
+	public void issue39() throws TernException {
+		TernDoc doc = createDoc("DiagApp");
+		MockTernAngularCompletionCollector collector = new MockTernAngularCompletionCollector();
+		server.request(doc, collector);
+
+		Assert.assertTrue(collector.getCompletions().size() == 1);
+		TernAngularCompletionItem item = collector.get("DiagCtrl");
+		Assert.assertNotNull(item);
+		Assert.assertEquals("DiagApp", item.getModule());
+		Assert.assertEquals("DiagCtrl", item.getName());
+		Assert.assertEquals(
+				"fn($scope: $scope, $location: service.$location, $timeout: fn(fn: fn(), delay?: number, invokeApply?: bool) -> Promise, $anchorScroll: fn())",
+				item.getType());
+		Assert.assertEquals("myfile.js", item.getOrigin());
+	}
+
+	private TernDoc createDoc(String module) {
+		String name = "myfile.js";
+
+		String text = "var diagModule	= angular.module('DiagModule', []);";
+		text += "\ndiagModule.controller('DiagCtrl', ['$scope', '$location', '$timeout', '$anchorScroll',";
+		text += "\nfunction($scope, $location, $timeout, $anchorScroll) {";
+		text += "\n}]);";
+		text += "\nvar diagapp	= angular.module('DiagApp', ['DiagModule']);";
+
+		TernDoc doc = new TernDoc();
+		doc.addFile(name, text, null);
+
+		TernAngularCompletionsQuery query = new TernAngularCompletionsQuery(
+				AngularType.controller);
+		query.getScope().setModule(module);
+		query.addFile("myfile.js");
+		query.setExpression("");
+
+		doc.setQuery(query);
+		return doc;
+	}
+}
