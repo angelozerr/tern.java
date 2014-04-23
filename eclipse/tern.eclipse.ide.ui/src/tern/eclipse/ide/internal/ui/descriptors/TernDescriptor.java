1a35ccce09042bff4a86bc675cd77f23cd3575bf
angelozerr <angelo.zerr@gmail.com>
2014-04-23 02:34:52 +0200

    Manage Icon of tern descriptor as optional.

--- eclipse/tern.eclipse.ide.ui/src/tern/eclipse/ide/internal/ui/descriptors/TernDescriptor.java
+++ eclipse/tern.eclipse.ide.ui/src/tern/eclipse/ide/internal/ui/descriptors/TernDescriptor.java
@@ -7,6 +7,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import tern.eclipse.ide.ui.ImageResource;
 import tern.eclipse.ide.ui.descriptors.ITernDescriptor;
+import tern.utils.StringUtils;
 
 public class TernDescriptor implements ITernDescriptor {
 
@@ -17,21 +18,25 @@ public class TernDescriptor implements ITernDescriptor {
 	public TernDescriptor(IConfigurationElement ce) {
 		this.id = ce.getAttribute("id");
 		this.description = ce.getAttribute("description");
-		this.imageKey = getImageKey(ce.getDeclaringExtension().getContributor()
-				.getName(), id);
-		String name = ce.getDeclaringExtension().getContributor().getName();
+		this.imageKey = getImageKey(ce, id);
+	}
+
+	private String getImageKey(IConfigurationElement ce, String id) {
 		String iconPath = ce.getAttribute("icon");
+		if (StringUtils.isEmpty(iconPath)) {
+			return null;
+		}
+		String name = ce.getDeclaringExtension().getContributor().getName();
+		String imageKey = new StringBuilder("ternDescriptor_").append(name)
+				.append(id).toString();
+
 		ImageDescriptor imageDescriptor = AbstractUIPlugin
 				.imageDescriptorFromPlugin(name, iconPath);
 		if (imageDescriptor == null && iconPath != null
 				&& iconPath.length() > 0)
 			imageDescriptor = ImageDescriptor.getMissingImageDescriptor();
 		ImageResource.registerImageDescriptor(imageKey, imageDescriptor);
-	}
-
-	private String getImageKey(String name, String id) {
-		return new StringBuilder("ternDescriptor_").append(name).append(id)
-				.toString();
+		return imageKey;
 	}
 
 	public String getId() {
@@ -45,6 +50,9 @@ public class TernDescriptor implements ITernDescriptor {
 
 	@Override
 	public Image getImage() {
+		if (imageKey == null) {
+			return null;
+		}
 		return ImageResource.getImage(imageKey);
 	}
 
