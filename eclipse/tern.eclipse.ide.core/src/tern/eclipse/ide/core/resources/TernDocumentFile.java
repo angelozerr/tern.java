5cd5244e99cdb8d2f5869202d70d04aeadf26c84
Piotr Tomiak <piotr@genuitec.com>
2014-10-20 10:37:19 +0200

    Updated license headers for Genuitec contribution.

--- eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/core/resources/TernDocumentFile.java
+++ eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/core/resources/TernDocumentFile.java
@@ -1,6 +1,12 @@
-/*
- * Copyright 2014, Genuitec, LLC
- * All Rights Reserved.
+/**
+ *  Copyright (c) 2014 Genuitec LLC.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Piotr Tomiak <piotr@genuitec.com> - initial API and implementation
  */
 package tern.eclipse.ide.core.resources;
 
--- eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/core/utils/FileUtils.java
+++ eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/core/utils/FileUtils.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2013-2014 Angelo ZERR.
+ *  Copyright (c) 2013-2014 Angelo ZERR and Genuitec LLC.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  *  Contributors:
  *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *  Piotr Tomiak <piotr@genuitec.com> - refactoring of file management API
  */
 package tern.eclipse.ide.core.utils;
 
