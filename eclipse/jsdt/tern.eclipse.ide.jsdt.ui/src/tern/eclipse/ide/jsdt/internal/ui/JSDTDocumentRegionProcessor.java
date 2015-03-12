f0afe6f94b7479283ab56d5d8855d3964e0ea247
angelozerr <angelo.zerr@gmail.com>
2015-03-12 22:33:26 +0100

    Don't use SSE spelling strategy inside JSDT Editor. See https://github.com/angelozerr/tern.java/issues/260

--- eclipse/jsdt/tern.eclipse.ide.jsdt.ui/src/tern/eclipse/ide/jsdt/internal/ui/JSDTDocumentRegionProcessor.java
+++ eclipse/jsdt/tern.eclipse.ide.jsdt.ui/src/tern/eclipse/ide/jsdt/internal/ui/JSDTDocumentRegionProcessor.java
@@ -1,13 +1,37 @@
+/**
+ *  Copyright (c) 2013-2015 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
 package tern.eclipse.ide.jsdt.internal.ui;
 
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.reconciler.IReconcilingStrategy;
 import org.eclipse.wst.sse.ui.internal.reconcile.DocumentRegionProcessor;
 
+/**
+ * Extends SSE {@link DocumentRegionProcessor} to be able to validate JSDT
+ * Editor content with "org.eclipse.wst.sse.ui.sourcevalidation" (see
+ * tern.eclipse.ide.linter.ui/plugin.xml) since JSDT Editor have not an SSE
+ * IStructuredModel.
+ * 
+ */
 public class JSDTDocumentRegionProcessor extends DocumentRegionProcessor {
 
 	@Override
 	public synchronized void startReconciling() {
 		super.startReconciling();
+	}
+
+	@Override
+	protected IReconcilingStrategy getSpellcheckStrategy() {
+		// don't use SSE spelling strategy.
+		return null;
 	}
 
 	@Override
