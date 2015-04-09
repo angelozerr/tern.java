a61d10e0a72313c7318d75b1e58e5773b54a77fc
angelozerr <angelo.zerr@gmail.com>
2015-04-09 18:32:58 +0200

    Provide simple tern text file.

---
+++ eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/core/resources/TernTextFile.java
@@ -0,0 +1,47 @@
+/**
+ *  Copyright (c) 2015 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
+package tern.eclipse.ide.core.resources;
+
+import java.io.IOException;
+
+import org.eclipse.core.resources.IFile;
+
+import tern.ITernFile;
+import tern.eclipse.ide.internal.core.resources.IDETernFile;
+
+public class TernTextFile extends IDETernFile implements ITernFile {
+
+	private String text;
+
+	public TernTextFile(IFile file, String text) {
+		super(file);
+		this.text = text;
+	}
+
+	@Override
+	public String getContents() throws IOException {
+		return text;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " [DOCUMENT]"; //$NON-NLS-1$
+	}
+
+	@Override
+	public Object getAdapter(@SuppressWarnings("rawtypes") Class adapterClass) {
+		if (adapterClass == String.class) {
+			return text;
+		}
+		return super.getAdapter(adapterClass);
+	}
+
+}
