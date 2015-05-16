cdf9fa6cde4e8cadf17dae54dd813f98f7c9ee35
angelozerr <angelo.zerr@gmail.com>
2015-05-16 20:40:26 +0200

    Integrate basic support for bootstrap. See https://github.com/angelozerr/tern.java/issues/280

---
+++ core/ternjs/node_modules/tern/node_modules/tern-bootstrap/bootstrap.js
@@ -0,0 +1,63 @@
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    return mod(require("tern/lib/infer"), require("tern/lib/tern"));
+  if (typeof define == "function" && define.amd) // AMD
+    return define([ "tern/lib/infer", "tern/lib/tern" ], mod);
+  mod(tern, tern);
+})(function(infer, tern) {
+  "use strict";
+
+  tern.registerPlugin("bootstrap", function(server, options) {
+
+    return {
+      defs : defs
+    };
+  });
+  
+  var defs = {
+  "!name": "bootstrap",
+  "!define": {
+    
+  },
+  "jQuery": {
+    "fn": {
+      "transition": {
+        "!type": "fn() -> !this"
+      },
+      "modal": {
+        "!type": "fn() -> !this"
+      },
+      "dropdown": {
+        "!type": "fn() -> !this"
+      },
+      "scrollspy": {
+        "!type": "fn() -> !this"
+      },
+      "tab": {
+        "!type": "fn() -> !this"
+      },
+      "tooltip": {
+        "!type": "fn() -> !this"
+      },
+      "popover": {
+        "!type": "fn() -> !this"
+      },
+      "alert": {
+        "!type": "fn() -> !this"
+      },
+      "button": {
+        "!type": "fn() -> !this"
+      },
+      "collapse": {
+        "!type": "fn() -> !this"
+      },
+      "carousel": {
+        "!type": "fn() -> !this"
+      },
+      "affix": {
+        "!type": "fn() -> !this"
+      }
+    }
+  }
+};
+});
