8c8df8b52e0e7af11d87939d79532bd1d269557e
angelozerr <angelo.zerr@gmail.com>
2015-07-03 13:48:37 +0200

    Integrates jQuery Extension. See https://github.com/angelozerr/tern.java/issues/296

---
+++ core/ternjs/node_modules/tern/node_modules/tern-jquery-extension/jquery-extension.js
@@ -0,0 +1,60 @@
+(function(root, mod) {
+    if (typeof exports == "object" && typeof module == "object") // CommonJS
+      return mod(exports, require("tern/lib/infer"), require("tern/lib/tern"));
+    if (typeof define == "function" && define.amd) // AMD
+        return define(["exports", "tern/lib/infer", "tern/lib/tern"], mod);
+    mod(root, tern, tern);
+})(this, function(exports, infer, tern) {
+    "use strict";
+  
+  infer.registerFunction("jQuery_querySelector", function(_self, args, argNodes) {
+    if (!argNodes || !argNodes.length || argNodes[0].type != "Literal" || typeof argNodes[0].value != "string" || !(argNodes[0].sourceFile && argNodes[0].sourceFile.dom)) {
+      return
+    }    
+    argNodes[0].cssselectors = true;
+  });
+  
+  // Lint
+    
+  function registerLints() {
+    if (!tern.registerLint) return;
+
+    // validate Element#querySelector/Element#queryAllSelector
+    tern.registerLint("jQuery_validateCSSSelectors", function(node, addMessage, getRule) {
+      var argNode = node.arguments[0];
+      if (!argNode || argNode.type != "Literal" || typeof argNode.value != "string") {       
+        return true;
+      }
+      var selector = argNode.value;
+      if ( selector.charAt(0) === "<" && selector.charAt( selector.length - 1 ) === ">" && selector.length >= 3 ) {
+        // Assume that strings that start and end with <> are HTML and skip the regex check
+        return;
+      }
+      // validate CSS selectors
+      tern.getLint("Browser_validateCSSSelectors")(node, addMessage, getRule);
+    });
+    
+  }
+    
+  tern.registerPlugin("jquery-extension", function(server, options) {
+    registerLints();
+    return {passes: {
+             preLoadDef: preLoadDef
+           }};
+  });
+
+  function selector(item) {
+    item["!effects"] = ["custom jQuery_querySelector"];
+    item["!data"] =  {"!lint": "jQuery_validateCSSSelectors"};        
+  }
+  
+  function preLoadDef(data) {    
+    var cx = infer.cx();
+    if (data["!name"] == "jQuery") {      
+      selector(data["jQuery"]);
+      data["jQuery"]["!type"] = "fn(selector: string|+Node|[+Node], context?: frameElement) -> jQuery.fn"
+    }
+  }
+  
+  
+})
