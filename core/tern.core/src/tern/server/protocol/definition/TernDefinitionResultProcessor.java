752e45652a3422d099f6c1fff3e53c9bdb8b8506
pascalleclercq <pascal.leclercq@gmail.com>
2015-08-30 22:42:40 +0200

    prepare release : update license header

--- core/tern.core/src/tern/server/protocol/definition/TernDefinitionResultProcessor.java
+++ core/tern.core/src/tern/server/protocol/definition/TernDefinitionResultProcessor.java
@@ -1,13 +1,12 @@
 /**
- *  Copyright (c) 2013-2015 Angelo ZERR and Genuitec LLC.
+ *  Copyright (c) 2013-2014 Angelo ZERR.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
  *
  *  Contributors:
- *  Angelo Zerr <angelo.zerr@gmail.com> - initial implementation
- *  Piotr Tomiak <piotr@genuitec.com> - collectors API and code refactoring
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  */
 package tern.server.protocol.definition;
 
--- eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/internal/core/resources/IDETernServerAsyncReqProcessor.java
+++ eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/internal/core/resources/IDETernServerAsyncReqProcessor.java
@@ -1,12 +1,12 @@
 /**
- *  Copyright (c) 2015 Genuitec LLC.
+ *  Copyright (c) 2013-2014 Angelo ZERR.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
  *
  *  Contributors:
- *  Piotr Tomiak <piotr@genuitec.com> - initial API and implementation
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  */
 package tern.eclipse.ide.internal.core.resources;
 
