d19bf95f77e3d551c9c111f610003aa04d5914aa
angelozerr <angelo.zerr@gmail.com>
2015-09-15 19:33:05 +0200

    Fix https://github.com/angelozerr/tern.java/issues/321

--- eclipse/tern.eclipse.ide.ui/src/tern/eclipse/ide/ui/controls/TernModulesBlock.java
+++ eclipse/tern.eclipse.ide.ui/src/tern/eclipse/ide/ui/controls/TernModulesBlock.java
@@ -13,6 +13,7 @@ package tern.eclipse.ide.ui.controls;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -398,7 +399,10 @@ public class TernModulesBlock extends AbstractTableBlock {
 	}
 
 	public Collection<ITernModule> getCheckedModules() {
-		return checkedModules;
+		if (checkedModules != null) {
+			return checkedModules;
+		}
+		return Collections.emptyList();
 	}
 
 	public void setCheckedModules(Collection<ITernModule> checkedModules) {
