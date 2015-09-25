2f104ce05903659af3ae6773209b00189429e55e
Piotr Tomiak <piotr@genuitec.com>
2015-09-25 14:49:26 +0200

    Updated EclipsePathAdapter to the latest Minimatch changes.

--- eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/internal/core/scriptpath/EclipsePathAdapter.java
+++ eclipse/tern.eclipse.ide.core/src/tern/eclipse/ide/internal/core/scriptpath/EclipsePathAdapter.java
@@ -10,10 +10,14 @@
  */
 package tern.eclipse.ide.internal.core.scriptpath;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import minimatch.Options;
 import minimatch.PathAdapter;
 
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 
 /**
  * Minimatch path adapter to use Minimatch with Eclipse {@link IPath}.
@@ -24,22 +28,16 @@ public class EclipsePathAdapter implements PathAdapter<IPath> {
 	public static final PathAdapter<IPath> INSTANCE = new EclipsePathAdapter();
 
 	@Override
-	public int getLength(IPath file) {
-		return file.segmentCount();
+	public List<String> toArray(IPath path, Options options) {
+		List<String> result = new ArrayList<String>();
+		if (path.isAbsolute()) {
+			result.add(""); //$NON-NLS-1$
+		}
+		result.addAll(Arrays.asList(path.segments()));
+		if (path.hasTrailingSeparator()) {
+			result.add(""); //$NON-NLS-1$
+		}
+		return result;
 	}
 
-	@Override
-	public String getPathName(IPath file, int i) {
-		return file.segment(i);
-	}
-
-	@Override
-	public IPath createPath(String filename) {
-		return new Path(filename);
-	}
-
-	@Override
-	public IPath subPath(IPath file, int fr) {
-		return file.removeFirstSegments(fr);
-	}
 }
