e36d97448c843af46aa7f2bbf8c4b66abe5999fa
angelozerr <angelo.zerr@gmail.com>
2015-10-16 00:55:56 +0200

    Initialize tern project editor. See https://github.com/angelozerr/tern.java/issues/358

---
+++ eclipse/tern.eclipse.ide.ui/src/tern/eclipse/ide/internal/ui/editors/TernProjectEditor.java
@@ -0,0 +1,51 @@
+/**
+- *  Copyright (c) 2013-2015 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
+package tern.eclipse.ide.internal.ui.editors;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.forms.editor.FormEditor;
+
+/**
+ * A multi page editor to manage .tern-project file.
+ *
+ */
+public class TernProjectEditor extends FormEditor {
+
+	private TextEditor editor;
+	
+	@Override
+	protected void addPages() {
+		try {
+			editor = new TextEditor();
+			super.addPage(editor, super.getEditorInput());
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		editor.doSave(monitor);
+	}
+
+	@Override
+	public void doSaveAs() {
+		editor.doSaveAs();
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		return editor.isSaveAsAllowed();
+	}
+
+}
---
+++ eclipse/tern.eclipse.ide.ui/src/tern/eclipse/ide/internal/ui/editors/TernProjectEditorContributor.java
@@ -0,0 +1,23 @@
+/**
+- *  Copyright (c) 2013-2015 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
+package tern.eclipse.ide.internal.ui.editors;
+
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.part.MultiPageEditorActionBarContributor;
+
+public class TernProjectEditorContributor extends MultiPageEditorActionBarContributor {
+
+	@Override
+	public void setActivePage(IEditorPart part) {
+		
+	}
+
+}
