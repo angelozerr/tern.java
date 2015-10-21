ab594ccbc9bdc29d4bcd2a73b4043b172c5afe7d
angelozerr <angelo.zerr@gmail.com>
2015-10-21 14:10:39 +0200

    Use last version of tern grunt.

--- core/ternjs/node_modules/tern-grunt/grunt.js
+++ core/ternjs/node_modules/tern-grunt/grunt.js
@@ -13,7 +13,7 @@
   }
   
   function getTasks(filename) {
-    var server = infer.cx().parent, data = server && server._grunt, gruntFile = filename && data[filename];
+    var server = infer.cx().parent, data = server && server.mod.grunt, gruntFile = filename && data[filename];
     return gruntFile ? gruntFile.tasks : null;
   }
   
@@ -25,7 +25,7 @@
   });
   
   infer.registerFunction("grunt_initConfig", function(self, args, argNodes) {
-    var server = infer.cx().parent, data = server && server._grunt;
+    var server = infer.cx().parent, data = server && server.mod.grunt;
     if (argNodes && argNodes.length && argNodes[0].type == "ObjectExpression") {
       var filename = argNodes[0].sourceFile.name, tasks = getTasks(filename);
       if (tasks) {
@@ -44,31 +44,26 @@
     return infer.ANull;
   });
   
-  tern.registerPlugin("grunt", function(server, options) {
-    
-    server._grunt = Object.create(null);
-    
-    server.on("afterLoad", function(file) {
-      if (isGrunfile(file.name)) {
-        var fnType = file.scope.exports && file.scope.exports.getFunctionType();
-        if (fnType) {
-          var cx = infer.cx(), deps = [cx.definitions.grunt.Grunt];
-          fnType.propagate(new infer.IsCallee(infer.cx().topScope, deps, null, infer.Null))
-        }
-      }
-    });
-    
-    return {
-      defs : defs,
-      passes: {
-          preInfer: preInfer          
-      }
-    };
+  tern.registerPlugin("grunt", function(server, options) {    
+    server.mod.grunt = Object.create(null);    
+    server.on("afterLoad", afterLoad);
+    server.on("preInfer", preInfer);
+    server.addDefs(defs);
   });
+  
+  function afterLoad(file) {
+    if (isGrunfile(file.name)) {
+      var fnType = file.scope.exports && file.scope.exports.getFunctionType();
+      if (fnType) {
+        var cx = infer.cx(), deps = [cx.definitions.grunt.Grunt];
+        fnType.propagate(new infer.IsCallee(infer.cx().topScope, deps, null, infer.Null))
+      }
+    }
+  }
 
   function preInfer(ast, scope) {
     var filename = ast.sourceFile.name;
-    if (isGrunfile(filename)) infer.cx().parent._grunt[filename] = {tasks: Object.create(null)};
+    if (isGrunfile(filename)) infer.cx().parent.mod.grunt[filename] = {tasks: Object.create(null)};
   }
   
   tern.defineQueryType("grunt-task", {
@@ -122,97 +117,137 @@
     "!name" : "grunt",
     "!define" : {
       Grunt : {
-        config : {
-          "!doc" : "Access project-specific configuration data defined in the Gruntfile.",
-          "!url" : "http://gruntjs.com/api/grunt.config",
-          init : {
-            "!type" : "fn(configObject: ?)",
-            "!doc" : "Initialize a configuration object for the current project.\nThe specified configObject is used by tasks and can be accessed using the grunt.config method.\nNearly every project's Gruntfile will call this method.",
-            "!url" : "http://gruntjs.com/api/grunt.config#grunt.config.init",
+        config: {
+          "!doc": "Access project-specific configuration data defined in the Gruntfile.",
+          "!url": "http://gruntjs.com/api/grunt.config",
+          init: {
+            "!type": "fn(configObject: ?)",
+            "!doc": "Initialize a configuration object for the current project.\nThe specified configObject is used by tasks and can be accessed using the grunt.config method.\nNearly every project's Gruntfile will call this method.",
+            "!url": "http://gruntjs.com/api/grunt.config#grunt.config.init",
             "!effects": ["custom grunt_initConfig"]
           },
-          get : {
-            "!type" : "fn(prop?: ?) -> ?",
-            "!doc" : "Get a value from the project's Grunt configuration. If prop is specified, that property's value is returned, or null if that property is not defined. If prop isn't specified, a copy of the entire config object is returned. Templates strings will be recursively processed using the grunt.config.process method.",
-            "!url" : "http://gruntjs.com/api/grunt.config#grunt.config.get"
+          get: {
+            "!type": "fn(prop?: ?) -> ?",
+            "!doc": "Get a value from the project's Grunt configuration. If prop is specified, that property's value is returned, or null if that property is not defined. If prop isn't specified, a copy of the entire config object is returned. Templates strings will be recursively processed using the grunt.config.process method.",
+            "!url": "http://gruntjs.com/api/grunt.config#grunt.config.get"
           },
-          process : {
-            "!type" : "fn(value?: ?) -> ?",
-            "!doc" : "Process a value, recursively expanding <% %> templates (via the grunt.template.process method) in the context of the Grunt config, as they are encountered. this method is called automatically by grunt.config.get but not by grunt.config.getRaw.",
-            "!url" : "http://gruntjs.com/api/grunt.config#grunt.config.process"
+          process: {
+            "!type": "fn(value?: ?) -> ?",
+            "!doc": "Process a value, recursively expanding <% %> templates (via the grunt.template.process method) in the context of the Grunt config, as they are encountered. this method is called automatically by grunt.config.get but not by grunt.config.getRaw.",
+            "!url": "http://gruntjs.com/api/grunt.config#grunt.config.process"
           },
-          getRaw : {
-            "!type" : "fn(value?: ?) -> ?",
-            "!doc" : "Get a raw value from the project's Grunt configuration, without processing <% %> template strings. If prop is specified, that property's value is returned, or null if that property is not defined. If prop isn't specified, a copy of the entire config object is returned.",
-            "!url" : "http://gruntjs.com/api/grunt.config#grunt.config.getRaw"
+          getRaw: {
+            "!type": "fn(value?: ?) -> ?",
+            "!doc": "Get a raw value from the project's Grunt configuration, without processing <% %> template strings. If prop is specified, that property's value is returned, or null if that property is not defined. If prop isn't specified, a copy of the entire config object is returned.",
+            "!url": "http://gruntjs.com/api/grunt.config#grunt.config.getRaw"
           },
-          set : {
-            "!type" : "fn(prop: ?, value: ?) -> ?",
-            "!doc" : "Set a value into the project's Grunt configuration.",
-            "!url" : "http://gruntjs.com/api/grunt.config#grunt.config.set"
+          set: {
+            "!type": "fn(prop: ?, value: ?) -> ?",
+            "!doc": "Set a value into the project's Grunt configuration.",
+            "!url": "http://gruntjs.com/api/grunt.config#grunt.config.set"
           },
-          escape : {
-            "!type" : "fn(propString: string) -> string",
-            "!doc" : "Escape . dots in the given propString. This should be used for property names that contain dots.",
-            "!url" : "http://gruntjs.com/api/grunt.config#grunt.config.escape"
+          escape: {
+            "!type": "fn(propString: string) -> string",
+            "!doc": "Escape . dots in the given propString. This should be used for property names that contain dots.",
+            "!url": "http://gruntjs.com/api/grunt.config#grunt.config.escape"
           },
-          requires : {
-            "!type" : "fn(prop: [string]) -> ?",
-            "!doc" : "Fail the current task if one or more required config properties is missing, null or undefined. One or more string or array config properties may be specified.",
-            "!url" : "http://gruntjs.com/api/grunt.config#grunt.config.requires"
+          requires: {
+            "!type": "fn(prop: [string]) -> ?",
+            "!doc": "Fail the current task if one or more required config properties is missing, null or undefined. One or more string or array config properties may be specified.",
+            "!url": "http://gruntjs.com/api/grunt.config#grunt.config.requires"
           }
         },
-        event : {
-          "!doc" : "Even though only the most relevant methods are listed on this page, the full EventEmitter2 API is available on the grunt.event object. Event namespaces may be specified with the . (dot) separator, and namespace wildcards have been enabled.",
-          "!url" : "http://gruntjs.com/api/grunt.event",
-          on : {
-            "!type" : "fn(event: Event, listener: ?)",
-            "!doc" : "Adds a listener to the end of the listeners array for the specified event.",
-            "!url" : "http://gruntjs.com/api/grunt.event#grunt.event.on"
+        event: {
+          "!doc": "Even though only the most relevant methods are listed on this page, the full EventEmitter2 API is available on the grunt.event object. Event namespaces may be specified with the . (dot) separator, and namespace wildcards have been enabled.",
+          "!url": "http://gruntjs.com/api/grunt.event",
+          on: {
+            "!type": "fn(event: Event, listener: ?)",
+            "!doc": "Adds a listener to the end of the listeners array for the specified event.",
+            "!url": "http://gruntjs.com/api/grunt.event#grunt.event.on"
           },
-          once : {
-            "!type" : "fn(event: Event, listener: ?)",
-            "!doc" : "Adds a one time listener for the event. The listener is invoked only the first time the event is fired, after which it is removed.",
-            "!url" : "http://gruntjs.com/api/grunt.event#grunt.event.once"
+          once: {
+            "!type": "fn(event: Event, listener: ?)",
+            "!doc": "Adds a one time listener for the event. The listener is invoked only the first time the event is fired, after which it is removed.",
+            "!url": "http://gruntjs.com/api/grunt.event#grunt.event.once"
           },
-          many : {
-            "!type" : "fn(event: Event, timesToListen: number, listener: ?)",
-            "!doc" : "Adds a listener that will execute n times for the event before being removed.",
-            "!url" : "http://gruntjs.com/api/grunt.event#grunt.event.many"
+          many: {
+            "!type": "fn(event: Event, timesToListen: number, listener: ?)",
+            "!doc": "Adds a listener that will execute n times for the event before being removed.",
+            "!url": "http://gruntjs.com/api/grunt.event#grunt.event.many"
           },
-          off : {
-            "!type" : "fn(event: Event, listener: ?)",
-            "!doc" : "Remove a listener from the listener array for the specified event.",
-            "!url" : "http://gruntjs.com/api/grunt.event#grunt.event.off"
+          off: {
+            "!type": "fn(event: Event, listener: ?)",
+            "!doc": "Remove a listener from the listener array for the specified event.",
+            "!url": "http://gruntjs.com/api/grunt.event#grunt.event.off"
           },
-          removeAllListeners : {
-            "!type" : "fn(event: [Event])",
-            "!doc" : "Removes all listeners, or those of the specified event.",
-            "!url" : "http://gruntjs.com/api/grunt.event#grunt.event.removealllisteners"
+          removeAllListeners: {
+            "!type": "fn(event: [Event])",
+            "!doc": "Removes all listeners, or those of the specified event.",
+            "!url": "http://gruntjs.com/api/grunt.event#grunt.event.removealllisteners"
           },
-          emit : {
-            "!type" : "fn(event: Event, arg: [?])",
-            "!doc" : "Execute each of the listeners that may be listening for the specified event name in order with the list of arguments.",
-            "!url" : "http://gruntjs.com/api/grunt.event#grunt.event.emit"
+          emit: {
+            "!type": "fn(event: Event, arg: [?])",
+            "!doc": "Execute each of the listeners that may be listening for the specified event name in order with the list of arguments.",
+            "!url": "http://gruntjs.com/api/grunt.event#grunt.event.emit"
           }
         },
-        initConfig : {
-          "!type" : "Grunt.config.init",
+        initConfig: {
+          "!type": "Grunt.config.init",
         },
-        task : {
-          "!doc" : "Register, run and load external tasks.",
-          "!url" : "http://gruntjs.com/api/grunt.task",
-          registerTask : {
-            "!type" : "fn(taskName: string, taskList: [string])",
-            "!doc" : "Register an 'alias task' or a task function. This method supports the following two signatures:",
-            "!url" : "http://gruntjs.com/api/grunt.task#grunt.task.registertask",
+        task: {
+          "!doc": "Register, run and load external tasks.",
+          "!url": "http://gruntjs.com/api/grunt.task",
+          registerTask: {
+            "!type": "fn(taskName: string, taskList: [string])",
+            "!doc": "Register an 'alias task' or a task function. This method supports the following two signatures:",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.registertask",
             "!effects": ["custom grunt_registerTask"]
           },
-          registerMultiTask : {
-            "!type" : "fn(taskName: string, description: string, taskFunction: fn())",
-            "!doc" : "Register an 'alias task' or a task function. This method supports the following two signatures:",
-            "!url" : "hhttp://gruntjs.com/api/grunt.task#grunt.task.registermultitask",
+          registerMultiTask: {
+            "!type": "fn(taskName: string, description: string, taskFunction: fn())",
+            "!doc": "Register an 'alias task' or a task function. This method supports the following two signatures:",
+            "!url": "hhttp://gruntjs.com/api/grunt.task#grunt.task.registermultitask",
             "!effects": ["custom grunt_registerTask"]
+          },
+          requires: {
+            "!type": "fn(taskName: string)",
+            "!doc": "Fail the task if some other task failed or never ran.",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.requires"
+          },
+          exists: {
+            "!type": "fn(taskName: string) -> bool",
+            "!doc": "Check with the name, if a task exists in the registered tasks. Return a boolean.",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.exists"
+          },
+          renameTask: {
+            "!type": "fn(oldname: string, newname: string) -> bool",
+            "!doc": "Rename a task. This might be useful if you want to override the default behavior of a task, while retaining the old name.",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.renametask"
+          },
+          loadTasks: {
+            "!type": "fn(tasksPath: string)",
+            "!doc": "Load task-related files from the specified directory, relative to the Gruntfile. This method can be used to load task-related files from a local Grunt plugin by specifying the path to that plugin's 'tasks' subdirectory.",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.loadtasks"
+          },
+          loadNpmTasks: {
+            "!type": "fn(pluginName: string)",
+            "!doc": "Load tasks from the specified Grunt plugin. This plugin must be installed locally via npm, and must be relative to the Gruntfile. Grunt plugins can be created by using the grunt-init gruntplugin template: grunt init:gruntplugin.",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.loadnpmtasks"
+          },
+          run: {
+            "!type": "fn(taskList: string|[string])",
+            "!doc": "Enqueue one or more tasks. Every specified task in taskList will be run immediately after the current task completes, in the order specified. The task list can be an array of tasks or individual task arguments.",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.run"
+          },
+          clearQueue: {
+            "!type": "fn()",
+            "!doc": "Empty the task queue completely. Unless additional tasks are enqueued, no more tasks will be run.",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.clearqueue"
+          },
+          normalizeMultiTaskFiles: {
+            "!type": "fn(data: ?, targetname?: string)",
+            "!doc": "Normalizes a task target configuration object into an array of src-dest file mappings. This method is used internally by the multi task system this.files / grunt.task.current.files property.",
+            "!url": "http://gruntjs.com/api/grunt.task#grunt.task.normalizemultitaskfiles"
           }
         },
         registerTask: {
@@ -221,19 +256,25 @@
         registerMultiTask: {
           "!type": "Grunt.task.registerMultiTask"
         },
-        fail : {
-          "!doc" : "For when something goes horribly wrong.",
-          "!url" : "http://gruntjs.com/api/grunt.fail",
-          warn : {
-            "!type" : "fn(error: Error, errorcode?: number)",
-            "!doc" : "Display a warning and abort Grunt immediately.",
-            "!url" : "http://gruntjs.com/api/grunt.fail#grunt.warn",
+        loadTasks: {
+          "!type": "Grunt.task.loadTasks"
+        },
+        loadNpmTasks: {
+          "!type": "Grunt.task.loadNpmTasks"
+        },
+        fail: {
+          "!doc": "For when something goes horribly wrong.",
+          "!url": "http://gruntjs.com/api/grunt.fail",
+          warn: {
+            "!type": "fn(error: Error, errorcode?: number)",
+            "!doc": "Display a warning and abort Grunt immediately.",
+            "!url": "http://gruntjs.com/api/grunt.fail#grunt.warn",
 
           },
-          fatal : {
-            "!type" : "fn(error: Error, errorcode?: number)",
-            "!doc" : "Display a warning and abort Grunt immediately.",
-            "!url" : "http://gruntjs.com/api/grunt.fail#grunt.fatal",
+          fatal: {
+            "!type": "fn(error: Error, errorcode?: number)",
+            "!doc": "Display a warning and abort Grunt immediately.",
+            "!url": "http://gruntjs.com/api/grunt.fail#grunt.fatal",
 
           }
         }
