1d1e9e45f093379fc2bcab069b02827ec31643af
angelozerr <angelo.zerr@gmail.com>
2016-01-16 17:48:23 +0100

    Add getResource utilities.

--- eclipse/tern.eclipse.ide.server.nodejs.core/src/tern/eclipse/ide/server/nodejs/core/debugger/VariableHelper.java
+++ eclipse/tern.eclipse.ide.server.nodejs.core/src/tern/eclipse/ide/server/nodejs/core/debugger/VariableHelper.java
@@ -1,6 +1,9 @@
 package tern.eclipse.ide.server.nodejs.core.debugger;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.variables.VariablesPlugin;
 
 public class VariableHelper {
@@ -10,5 +13,13 @@ public class VariableHelper {
 	public static String getWorkspaceLoc(IResource file) {
 		return VariablesPlugin.getDefault().getStringVariableManager().generateVariableExpression(WORKSPACE_LOC,
 				file.getFullPath().toString());
+	}
+
+	public static IFile getResource(String file) {
+		int index = file.indexOf(WORKSPACE_LOC);
+		if (index > 0) {
+			file = file.substring(index + WORKSPACE_LOC.length() + 1, file.length() - 1);
+		}
+		return ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(file));
 	}
 }
