9f18c7b25b96ec2ebd022244675148a0d2b4fe2b
angelozerr <angelo.zerr@gmail.com>
2016-01-17 10:18:53 +0100

    Add getFileForLocation

--- eclipse/tern.eclipse.ide.server.nodejs.core/src/tern/eclipse/ide/server/nodejs/core/debugger/VariableHelper.java
+++ eclipse/tern.eclipse.ide.server.nodejs.core/src/tern/eclipse/ide/server/nodejs/core/debugger/VariableHelper.java
@@ -1,11 +1,27 @@
+/**
+ *  Copyright (c) 2013-2016 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
 package tern.eclipse.ide.server.nodejs.core.debugger;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.variables.VariablesPlugin;
 
+/**
+ * Variable helper.
+ *
+ */
 public class VariableHelper {
 
 	private static final String WORKSPACE_LOC = "workspace_loc";
@@ -15,11 +31,21 @@ public class VariableHelper {
 				file.getFullPath().toString());
 	}
 
-	public static IFile getResource(String file) {
-		int index = file.indexOf(WORKSPACE_LOC);
-		if (index > 0) {
-			file = file.substring(index + WORKSPACE_LOC.length() + 1, file.length() - 1);
+	public static IFile getResource(String path) throws CoreException {
+		String location = VariablesPlugin.getDefault().getStringVariableManager().performStringSubstitution(path);
+		return getFileForLocation(location);
+	}
+
+	public static IFile getFileForLocation(String path) {
+		if (path == null) {
+			return null;
 		}
-		return ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(file));
+		IPath filePath = new Path(path);
+		IFile file = null;
+		IFile[] files = ResourcesPlugin.getWorkspace().getRoot().findFilesForLocation(filePath);
+		if (files.length > 0) {
+			return files[0];
+		}
+		return null;
 	}
 }
